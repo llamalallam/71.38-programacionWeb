8ecd5d7ab8f6013e9447ca99896a1c1aadcfddc5
Cristi Haug <mhaug@itba.edu.ar>
2023-11-17 01:12:38 -0300

    Diseño de la nav bar pasado en limpio

---
+++ hola.js
@@ -0,0 +1,10 @@
+let edad = 12;
+if (edad>18){
+    console.log("proceda");
+}else if (edad<18){
+    console.log("huya");
+}
+else if (typeof edad!=number){
+    console.log("valor no valido")
+}
+else{console.log("error")}
